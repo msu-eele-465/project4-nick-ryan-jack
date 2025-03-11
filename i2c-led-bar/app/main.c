f4dcb0e117dd59bcfc1516c20e4065a316d17809
Shintaru87 <nickdhaldar@gmail.com>
2025-03-11 13:26:01 -0600

    LEDBAR to a point where almost all patterns work, I2c not set up yet

--- i2c-led-bar/app/main.c
+++ i2c-led-bar/app/main.c
@@ -14,50 +14,55 @@ update the value of the timer's CCR0 to have a higher and lower frequency.
 */
 
 
+// SLAVE ADDRESS 0x69
+
+
 
 int current_value;
 
-int Upper_Lightbar_array0 = 0b00001010;
-int Lower_Lightbar_array0 = 0b10100000;
+int P1_Lightbar_array0 = 0b10100000;
+int P2_Lightbar_array0 = 0b00001010;
 
-int Upper_Lightbar_array1[] = {0b10100000, 0b01010000};
-int Lower_Lightbar_array1[] = {0b00001010, 0b00000101};
+int P1_Lightbar_array1[] = {0b10100010, 0b01010001};
+int P2_Lightbar_array1[] = {0b10000000, 0b01000000};
 int LIGHTBAR1_LENGTH = 1;
 int LIGHTBAR1_POS;
 
-int Upper_Lightbar_value2;
-int Lower_Lightbar_value2;
+int P1_Lightbar_value2;
+int P2_Lightbar_value2;
 int LIGHTBAR2_LENGTH = 15;
 int LIGHTBAR2_POS; // instead of doing this just increment P6 by one 16 times, and increment P4 by 16 every 16 times
 
-int Upper_Lightbar_array3[] = {0b00010000, 0b00100000, 0b01000000, 0b10000000, 0b01000000, 0b00100000};
-int Lower_Lightbar_array3[] = {0b00001000, 0b00000100, 0b00000010, 0b00000001, 0b00000010, 0b00000100};
+int P1_Lightbar_array3[] = {0b00010000, 0b00100000, 0b01000010, 0b10000001, 0b01000010, 0b00100000};
+int P2_Lightbar_array3[] = {0b10000000, 0b01000000, 0b00000000, 0b00000000, 0b00000000, 0b01000000};
 int LIGHTBAR3_LENGTH = 5;
 int LIGHTBAR3_POS;
 
-int Upper_Lightbar_value4;
-int Lower_Lightbar_value4;
+int P1_Lightbar_value4;
+int P2_Lightbar_value4;
 int LIGHTBAR4_LENGTH = 15;
 int LIGHTBAR4_POS; // instead of doing this just decrement P6 by one 16 times, and decrement P4 by 16 every 16 times
 
-int Upper_Lightbar_array5[] = {0b00000000, 0b00000000, 0b00000000, 0b00000000, 0b00010000, 0b00100000, 0b01000000, 0b10000000};
-int Lower_Lightbar_array5[] = {0b00000001, 0b00000010, 0b00000100, 0b00001000, 0b00000000, 0b00000000, 0b00000000, 0b00000000};
+int P1_Lightbar_array5[] = {0b00000001, 0b00000010, 0b00000000, 0b00000000, 0b00010000, 0b00100000, 0b01000000, 0b10000000};
+int P2_Lightbar_array5[] = {0b00000000, 0b00000000, 0b01000000, 0b10000000, 0b00000000, 0b00000000, 0b00000000, 0b00000000};
 int LIGHTBAR5_LENGTH = 7;
 int LIGHTBAR5_POS;
 
-int Upper_Lightbar_array6[] = {0b01110000, 0b10110000, 0b11010000, 0b11100000, 0b11110000, 0b11110000, 0b11110000, 0b11110000};
-int Lower_Lightbar_array6[] = {0b00001111, 0b0001111, 0b00001111, 0b00001111, 0b00000111, 0b00001011, 0b00001101, 0b00001110};
+int P1_Lightbar_array6[] = {0b01110011, 0b10110011, 0b11010011, 0b11100011, 0b11110011, 0b11110011, 0b11110001, 0b11110010};
+int P2_Lightbar_array6[] = {0b11000000, 0b1100000, 0b11000000, 0b11000000, 0b01000000, 0b10000000, 0b11000000, 0b11000000};
 int LIGHTBAR6_LENGTH = 7;
 int LIGHTBAR6_POS;
 
-int Upper_Lightbar_array7[] = {0b00000000, 0b00000000, 0b00000000, 0b00000000, 0b00010000, 0b00110000, 0b01110000, 0b11110000};
-int Lower_Lightbar_array7[] = {0b00000001, 0b0000011, 0b00000111, 0b00001111, 0b00001111, 0b00001111, 0b00001111, 0b00001111};
+int P1_Lightbar_array7[] = {0b00000001, 0b00000011, 0b00000011, 0b00000011, 0b00010011, 0b00110011, 0b01110011, 0b11110011};
+int P2_Lightbar_array7[] = {0b00000000, 0b0000000, 0b01000000, 0b11000000, 0b11000000, 0b11000000, 0b11000000, 0b11000000};
 int LIGHTBAR7_LENGTH = 7;
 int LIGHTBAR7_POS;
 
 int delay_var;
 
-int input = 4; 
+char input = '6'; 
+
+int rx_val; 
 
 
 
@@ -71,25 +76,33 @@ void Initialize_LEDBAR()
    // Stop watchdog timer
    WDTCTL = WDTPW | WDTHOLD;
 
+    //----- Setup I2C to be in SLAVE mode (0x69)--------
+
+        UCB0CTLW0 = UCSWRST;                      // Software reset enabled
+        UCB0CTLW0 |= UCMODE_3 | UCSYNC;           // I2C mode, sync mode
+        UCB0I2COA0 = 0x69 | UCOAEN;               // Own Address and enable
+        UCB0CTLW0 &= ~UCSWRST;                    // clear reset register
+        //UCB0IE |= UCRXIE + UCSTPIE;
+
+    //----- Finished I2C INIT--------
+
 
     //----- Setup light bar output pins ((BIT0 & BIT1) P1.0 -> P1.1, (BIT2 & BIT3) P2.6 -> P2.7, (Upper 4 BITS) P1.4 -> P1.7 )--------
-        //P6SEL0 |= 0b00011111;   // P1.0 -> P1.1
-        //P6SEL1 |= 0b00011111;   // make IO
-        P1DIR |= 0b00000011;    // make it output
-        P1REN &= ~0b00000011;   // give pull down resistor
-        P1OUT &= ~0b00000011;   // clear output
-
+    
         //P6SEL0 |= 0b00011111;   // P2.6 -> P2.7
         //P6SEL1 |= 0b00011111;   // make IO
-        P1DIR |= 0b11000000;    // make it output
-        P1REN &= ~0b11000000;   // give pull down resistor
-        P1OUT &= ~0b11000000;   // clear output
+        P2DIR |= 0b11000001;    // make it output
+        //P2REN &= ~0b11000000;   // give pull down resistor
+        P2OUT &= ~0b11000001;   // clear output
         
-        //P3SEL0 |= 0b10001101;   // P1.4 -> P1.7
+        //P3SEL0 |= 0b10001101;   // P1.4 -> P1. for upper. P1.0 and P1.1 for lower
         //P3SEL1 |= 0b10001101;
-        P1DIR |= 0b11110000;
-        P1REN &= ~0b11110000;
-        P1OUT &= ~0b11110000;
+        P1DIR |= 0b11110011;
+        //P1REN &= ~0b11110011;
+        P1OUT &= ~0b11110011;
+
+
+        PM5CTL0 &=  ~LOCKLPM5;  // Turn on GPIO
 
     // ------- light bar done -----
 
@@ -97,8 +110,8 @@ void Initialize_LEDBAR()
         TB0CTL |= TBCLR;            // Clear Timers and Dividers
         TB0CTL |= TBSSEL__SMCLK;    // Source = SMCLK
         TB0CTL |= MC__UP;           // Mode   = Up
-        TB0CCR0 = 1000;             // TB0CCR0 = 1000
-        TB0CCR1 = 250;             // TB0CCR1 = 0.25 * TB0CCR0 = 250
+        TB0CCR0 = 50000;            // TB0CCR0 = 1000
+        TB0CCR1 = 250;              // TB0CCR1 = 0.25 * TB0CCR0 = 250
 
     //----- Setup Timers Overflow IQR--------
         TB0CCTL0 &= ~CCIFG;           //Clear CCR0 flag
@@ -120,114 +133,9 @@ int main()
     //R5 |= 0b00000000;
     //R6 |= 0b00000000;
 
-
-    
-
-   // Stop watchdog timer
-   WDTCTL = WDTPW | WDTHOLD;               // Stop watchdog timer
-  
-   // Disable GPIO power-on default high-impedance mode
-   PM5CTL0 &= ~LOCKLPM5;
+Initialize_LEDBAR();
 
 while(true){
-        //delay_loop();
-        switch(input) {
-            case '0':
-                P6OUT = Lower_Lightbar_array0;
-                P4OUT = Upper_Lightbar_array0;
-
-                break;
-           case '1':
-                if(LIGHTBAR1_POS == LIGHTBAR1_LENGTH){ 
-                    P6OUT = Lower_Lightbar_array1[LIGHTBAR1_POS];
-                    P4OUT = Upper_Lightbar_array1[LIGHTBAR1_POS];
-                    LIGHTBAR1_POS = 0;
-                    break;
-                }
-                P6OUT = Lower_Lightbar_array1[LIGHTBAR1_POS];
-                P4OUT = Upper_Lightbar_array1[LIGHTBAR1_POS];
-                LIGHTBAR1_POS++;
-                break;
-           case '2': 
-                if(LIGHTBAR2_POS == LIGHTBAR2_LENGTH){ 
-                    if(Upper_Lightbar_value2 == 0b11110000){
-                        Upper_Lightbar_value2 = 0;
-                    }else{Upper_Lightbar_value2 += 16;}
-                    Lower_Lightbar_value2 = 0;
-                    P6OUT = Lower_Lightbar_value2;
-                    P4OUT = Upper_Lightbar_value2;
-                    LIGHTBAR2_POS = 0;
-                    break;
-                }
-                P6OUT = Lower_Lightbar_value2;
-                Lower_Lightbar_value2 ++;
-                LIGHTBAR2_POS++;
-                break;
-
-           case '3':
-                if(LIGHTBAR3_POS == LIGHTBAR3_LENGTH){ 
-                    P6OUT = Lower_Lightbar_array3[LIGHTBAR3_POS];
-                    P4OUT = Upper_Lightbar_array3[LIGHTBAR3_POS];
-                    LIGHTBAR3_POS = 0;
-                    break;
-                }
-                P6OUT = Lower_Lightbar_array3[LIGHTBAR3_POS];
-                P4OUT = Upper_Lightbar_array3[LIGHTBAR3_POS];
-                LIGHTBAR3_POS++;
-                break;
-
-            case '4': 
-                if(Lower_Lightbar_value4 == 0b00000000 && LIGHTBAR4_POS == 0 && Upper_Lightbar_value4 == 0b11110000){
-                     Upper_Lightbar_value4 = 0b11110000;}
-                if(LIGHTBAR4_POS == LIGHTBAR4_LENGTH){ 
-                    if(Lower_Lightbar_value4 == 0b00000000){
-                        Upper_Lightbar_value4 = 0b11110000;
-                    }else{Upper_Lightbar_value4 -= 16;}
-                    Lower_Lightbar_value4 = 0b00001111;
-                    P6OUT = Lower_Lightbar_value4;
-                    P4OUT = Upper_Lightbar_value4;
-                    LIGHTBAR4_POS = 0;
-                    break;
-                }
-                Lower_Lightbar_value4 --;
-                P6OUT = Lower_Lightbar_value4;
-                LIGHTBAR4_POS++;
-                break;
-            case '5':
-                if(LIGHTBAR5_POS == LIGHTBAR5_LENGTH){ 
-                    P6OUT = Lower_Lightbar_array5[LIGHTBAR5_POS];
-                    P4OUT = Upper_Lightbar_array5[LIGHTBAR5_POS];
-                    LIGHTBAR5_POS = 0;
-                    break;
-                }
-                P6OUT = Lower_Lightbar_array5[LIGHTBAR5_POS];
-                P4OUT = Upper_Lightbar_array5[LIGHTBAR5_POS];
-                LIGHTBAR5_POS++;
-                break;
-
-            case '6':
-                if(LIGHTBAR6_POS == LIGHTBAR6_LENGTH){ 
-                    P6OUT = Lower_Lightbar_array6[LIGHTBAR6_POS];
-                    P4OUT = Upper_Lightbar_array6[LIGHTBAR6_POS];
-                    LIGHTBAR6_POS = 0;
-                    break;
-                }
-                P6OUT = Lower_Lightbar_array6[LIGHTBAR6_POS];
-                P4OUT = Upper_Lightbar_array6[LIGHTBAR6_POS];
-                LIGHTBAR6_POS++;
-                break;
-            case '7':
-                if(LIGHTBAR7_POS == LIGHTBAR7_LENGTH){ 
-                    P6OUT = Lower_Lightbar_array7[LIGHTBAR7_POS];
-                    P4OUT = Upper_Lightbar_array7[LIGHTBAR7_POS];
-                    LIGHTBAR7_POS = 0;
-                    break;
-                }
-                P6OUT = Lower_Lightbar_array7[LIGHTBAR7_POS];
-                P4OUT = Upper_Lightbar_array7[LIGHTBAR7_POS];
-                LIGHTBAR7_POS++;
-                break;
-       }; 
 
     }
 }
@@ -242,7 +150,106 @@ while(true){
 */
 #pragma vector = TIMER0_B0_VECTOR
 __interrupt void ISR_TB0_CCR0(void){
-    input++;
+    switch(input) {
+            case '0':
+                P2OUT = P2_Lightbar_array0;
+                P1OUT = P1_Lightbar_array0;
+
+                break;
+           case '1':
+                if(LIGHTBAR1_POS == LIGHTBAR1_LENGTH){ 
+                    P2OUT = P2_Lightbar_array1[LIGHTBAR1_POS];
+                    P1OUT = P1_Lightbar_array1[LIGHTBAR1_POS];
+                    LIGHTBAR1_POS = 0;
+                    break;
+                }
+                P2OUT = P2_Lightbar_array1[LIGHTBAR1_POS];
+                P1OUT = P1_Lightbar_array1[LIGHTBAR1_POS];
+                LIGHTBAR1_POS++;
+                break;
+           case '2':    //----------------- FIX
+                if(LIGHTBAR2_POS == LIGHTBAR2_LENGTH){ 
+                    if(P1_Lightbar_value2 == 0b11110000){
+                        P1_Lightbar_value2 = 0;
+                    }else{P1_Lightbar_value2 += 16;}
+                    P2_Lightbar_value2 = 0;
+                    P2OUT = P2_Lightbar_value2;
+                    P1OUT = P1_Lightbar_value2;
+                    LIGHTBAR2_POS = 0;
+                    break;
+                }
+                P2OUT = P2_Lightbar_value2;
+                P2_Lightbar_value2 ++;
+                LIGHTBAR2_POS++;
+                break;
+
+           case '3':
+                if(LIGHTBAR3_POS == LIGHTBAR3_LENGTH){ 
+                    P2OUT = P2_Lightbar_array3[LIGHTBAR3_POS];
+                    P1OUT = P1_Lightbar_array3[LIGHTBAR3_POS];
+                    LIGHTBAR3_POS = 0;
+                    break;
+                }
+                P2OUT = P2_Lightbar_array3[LIGHTBAR3_POS];
+                P1OUT = P1_Lightbar_array3[LIGHTBAR3_POS];
+                LIGHTBAR3_POS++;
+                break;
+
+            case '4':  //----------------- FIX
+                if(P2_Lightbar_value4 == 0b00000000 && LIGHTBAR4_POS == 0 && P1_Lightbar_value4 == 0b11110000){
+                     P1_Lightbar_value4 = 0b11110000;}
+                if(LIGHTBAR4_POS == LIGHTBAR4_LENGTH){ 
+                    if(P2_Lightbar_value4 == 0b00000000){
+                        P1_Lightbar_value4 = 0b11110000;
+                    }else{P1_Lightbar_value4 -= 16;}
+                    P2_Lightbar_value4 = 0b00001111;
+                    P2OUT = P2_Lightbar_value4;
+                    P1OUT = P2_Lightbar_value4;
+                    LIGHTBAR4_POS = 0;
+                    break;
+                }
+                P2_Lightbar_value4 --;
+                P2OUT = P2_Lightbar_value4;
+                LIGHTBAR4_POS++;
+                break;
+            case '5':
+                if(LIGHTBAR5_POS == LIGHTBAR5_LENGTH){ 
+                    P2OUT = P2_Lightbar_array5[LIGHTBAR5_POS];
+                    P1OUT = P1_Lightbar_array5[LIGHTBAR5_POS];
+                    LIGHTBAR5_POS = 0;
+                    break;
+                }
+                P2OUT = P2_Lightbar_array5[LIGHTBAR5_POS];
+                P1OUT = P1_Lightbar_array5[LIGHTBAR5_POS];
+                LIGHTBAR5_POS++;
+                break;
+
+            case '6':
+                if(LIGHTBAR6_POS == LIGHTBAR6_LENGTH){ 
+                    P2OUT = P2_Lightbar_array6[LIGHTBAR6_POS];
+                    P1OUT = P1_Lightbar_array6[LIGHTBAR6_POS];
+                    LIGHTBAR6_POS = 0;
+                    break;
+                }
+                P2OUT = P2_Lightbar_array6[LIGHTBAR6_POS];
+                P1OUT = P1_Lightbar_array6[LIGHTBAR6_POS];
+                LIGHTBAR6_POS++;
+                break;
+            case '7':
+                if(LIGHTBAR7_POS == LIGHTBAR7_LENGTH){ 
+                    P2OUT = P2_Lightbar_array7[LIGHTBAR7_POS];
+                    P1OUT = P1_Lightbar_array7[LIGHTBAR7_POS];
+                    LIGHTBAR7_POS = 0;
+                    break;
+                }
+                P2OUT = P2_Lightbar_array7[LIGHTBAR7_POS];
+                P1OUT = P1_Lightbar_array7[LIGHTBAR7_POS];
+                LIGHTBAR7_POS++;
+                break;
+
+            default:
+                break;
+       };
     TB0CCTL0 &= ~CCIFG; // clearing CCR0 ISR flag
 }
 /*
@@ -255,11 +262,22 @@ __interrupt void ISR_TB0_CCR0(void){
 */
 #pragma vector = TIMER0_B1_VECTOR
 __interrupt void ISR_TB0_CCR1(void){
+    P2OUT ^ BIT0; 
     TB0CCTL1 &= ~CCIFG; // clearing CCR1 ISR flag
 }
 /*
 * END Timer0_B1 ISR
 */
+
+
+
+
+#pragma vector = USCI_B0_VECTOR
+__interrupt void USCI_B0_ISR(void){
+  //Must read from UCB0RXBUF
+  rx_val = 0;
+  rx_val = UCB0RXBUF;
+}
 
 
 
